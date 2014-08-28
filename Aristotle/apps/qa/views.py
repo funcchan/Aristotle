582615df7f19c93c42fc26a361ec58f2e43c8adf
lljllj122 <lljllj122@gmail.com>
2014-08-28 16:20:29 -0700

    Merge branch 'master' of https://github.com/funcchan/Aristotle
    Conflicts:
    	Aristotle/apps/qa/views.py
    	Aristotle/urls.py

--- Aristotle/apps/qa/views.py
+++ Aristotle/apps/qa/views.py
@@ -4,24 +4,31 @@
 # @create: Aug. 25th, 2014
 # @update: Aug. 28th, 2014
 # @author: hitigon@gmail.com
-# from django.http import HttpResponse
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.views.generic import View
-from errors import InvalidFieldError
+from .errors import InvalidFieldError
 
 
 class HomeView(View):
-
     def get(self, request, *args, **kwargs):
         return render(request, 'qa/index.html')
 
 
 class SignInView(View):
-
     def get(self, request, *args, **kwargs):
+        # TODO: Check the session
+        """
+        Verify the session.
+            - If already login, redirect to the home page.
+        :param request:
+        :param args:
+        :param kwargs:
+        :return:
+        """
         return render(request, 'qa/signin.html')
 
     def post(self, request, *args, **kwargs):
@@ -38,8 +45,16 @@ class SignInView(View):
 
 
 class SignUpView(View):
-
     def get(self, request, *args, **kwargs):
+        # TODO: Check the session
+        """
+        Verify the session.
+            - If already login, redirect to the home page.
+        :param request:
+        :param args:
+        :param kwargs:
+        :return:
+        """
         return render(request, 'qa/signup.html')
 
     def post(self, request, *args, **kwargs):
@@ -48,11 +63,11 @@ class SignUpView(View):
         password = request.POST['password']
         repassword = request.POST['repassword']
         err_msgs = []
-        if not username:
+        if not username:  # TODO: Validate the username
             err_msgs.append('Username is required')
-        if not email:
+        if not email:  # TODO: Validate the email
             err_msgs.append('Email is required')
-        if not password or not repassword:
+        if not password or not repassword:  # TODO: Validate the pwd
             err_msgs.append('Passwords are required')
         if password != repassword:
             err_msgs.append('Two passwords are not identical')
@@ -73,7 +88,53 @@ class SignUpView(View):
 
 
 class SignOutView(View):
-
     def get(self, request, *args, **kwargs):
         logout(request)
         return redirect('/signin')
+
+
+# Start by Liangju
+# @ Aug. 28th, 2014
+class UserSettingView(View):
+
+    def get(self, request, *args, **kwargs):
+        # TODO: View for user info
+        """
+        1. Retrieval the User data by User ID
+            - If no such user. Redirect to non-existing page.
+
+        2. Check the current user login status
+            - If it is the page for current user, there should be more links, such as Edit, My Question, Modification, etc.
+            - If it is the page for other users, just show the necessary information.
+
+        3. Render the page
+
+        :param request:
+        :return:
+        """
+        return render(request, 'qa/user_setting.html',
+                      {'user_id': self.kwargs['user_id']})
+
+class QuestionView(View):
+
+    def get(self, request, *args, **kwargs):
+        # TODO: View for the question content
+        """
+        1. Search the question with question_id.
+            - If not valid, redirect to 404
+
+        2. Retrieval contents
+            - Answers,
+            - Users,
+            - Comments,
+            - Appends,
+            - Tags.
+
+        3. Render the page
+
+        :param request:
+        :param question_id:
+        :return:
+        """
+        return render(request, 'qa/question.html',
+                      {'question_id': self.kwargs['question_id']})
